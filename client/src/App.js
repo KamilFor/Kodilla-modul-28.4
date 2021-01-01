4930c7c85f91f04b5142e66fe180b1abf2be5a48
Kamil Fornalski <kester.kamil@gmail.com>
2021-01-01 17:49:00 +0100

    28.4 do poprawy

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.socket = io();
+    this.socket = io('http://localhost:8000');
     // state
     this.state = {
-      tasks: [{ id: 1, name: 'Shopping' }, { id: 2, name: 'Go out with dog' }],
+      tasks: [{ id: 1, name: 'Shopping' }],
+      taskName: '',
     };
+
+    // Global Methods
   }
   // Render function
   render() {
-    //Methods
+    // Methods
+
+    // Remove Task (Initiate in Button inside Tasks.Map function)
     const removeTask = (id) => {
       this.setState((state, props) => {
         return (state.tasks = state.tasks.filter((task) => task.id !== id));
       });
+      this.socket.emit('removeTask', id);
     };
 
+    // Submit Form ( Initiate in Add Form onSubmit event)
+    const submitForm = (e) => {
+      e.preventDefault();
+      addTask(this.state.taskName);
+      this.socket.emit('addTask', this.state.taskName);
+    };
+
+    // Add task function which only create task !
+    const addTask = (task) => {
+      this.setState((state, props) => {
+        return state.tasks.push({ id: uuidv4(), name: task });
+      });
+    };
+
+    // App Component
     return (
       <div className='App'>
         <header>
@@ -42,13 +64,18 @@ class App extends React.Component {
             })}
           </ul>
 
-          <form id='add-task-form'>
+          <form id='add-task-form' onSubmit={(e) => submitForm(e)}>
             <input
               className='text-input'
               autoComplete='off'
               type='text'
               placeholder='Type your description'
               id='task-name'
+              value={this.state.taskName}
+              onChange={(e) =>
+                this.setState((state, props) => {
+                  return (state.taskName = e.target.value);
+                })}
             />
             <button className='btn' type='submit'>
               Add
@@ -58,7 +85,36 @@ class App extends React.Component {
       </div>
     );
   }
-  componentDidMount() {}
+  // Socket IO listeners
+  componentDidMount() {
+    // Add task function which only create task ! Nie wiem jak użyć metody z render w Component Did mount
+
+    // Methods from render
+    const addTask = (task) => {
+      this.setState((state, props) => {
+        return state.tasks.push({ id: uuidv4(), name: task });
+      });
+    };
+    this.socket.on('addTask', (name) => addTask(name));
+
+    const removeTask = (id) => {
+      this.setState((state, props) => {
+        return (state.tasks = state.tasks.filter((task) => task.id !== id));
+      });
+    };
+
+    this.socket.on('removeTask', (id) => removeTask(id));
+  }
+  /*
+    this.socket.emit('updateData', this.state.tasks);
+    this.socket.on('updateData', (tasks) => {
+      this.setState((state, props) => {
+        tasks.map((task) => {
+          state.tasks.push(task);
+        });
+      });
+    });
+    */
 }
 
 export default App;
--- server.js
+++ server.js
@@ -11,21 +11,38 @@ const io = socketio(server);
 // Global Variables
 const tasks = [];
 
+app.get('/', (req, res) => {
+  res.send('index');
+});
+
 // Static Folder
 app.use(express.static(path.join(__dirname, 'client')));
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Error 404 not found' });
-});
 
 // Server IO Connection
 io.on('connection', (socket) => {
   console.log('New user joined');
-  // Emit UpdateData
+
+  // Update Data
   socket.emit('updateData', tasks);
-  // Listener AddTask i RemoveTask
-  socket.on('addTask', { taskName, eventTask });
-  socket.on('removeTask', { taskId, eventTask });
+
+  socket.on('updateData', (localTasks) => {
+    // Trzeba poprawić ... dubluje się ..
+    localTasks.map((item) => tasks.push(item));
+  });
+
+  //   // Listener AddTask i RemoveTask
+  socket.on('addTask', (taskName) => {
+    socket.broadcast.emit('addTask', taskName);
+  });
+
+  socket.on('removeTask', (id) => {
+    socket.broadcast.emit('removeTask', id);
+  });
+  //   console.log(taskId);
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Error 404 not found' });
 });
 
 // Server runing
